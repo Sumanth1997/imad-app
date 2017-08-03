476dfe79baecbb55ddd4b4599bc22709a8cdee17
Sumanth1997 <sumanthmylar@gmail.com>
2017-08-04 01:41:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ var htmlTemplate=`
 <!DOCTYPE html>
     <head>
         <title>
-            $(title)
+            ${title}
         </title>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link href="/ui/style.css" rel="stylesheet" />
@@ -46,13 +46,13 @@ var htmlTemplate=`
                 </div>
                 <hr/>
                 <h3>
-                    $(heading)
+                    ${heading}
                 </h3>
                 <div>
-                    $(date)
+                    ${date}
                 </div>
                 <div>
-                    $(content)
+                    ${content}
                 </div>
         </div>
     </body>
