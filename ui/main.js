bbc5e4f0eaa088000594cddcd62daf1de5fca7a3
Sumanth1997 <sumanthmylar@gmail.com>
2017-08-05 01:13:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,3 +8,8 @@ var element=document.getElementById('main-text'
 
 element.innerHTML = 'new value';
 
+//move the element
+var img = document.getElementById('madi');
+   img.onclick = function () {
+       img.style.marginLeft = '100px';
+   };
