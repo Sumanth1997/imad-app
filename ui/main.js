c9c949896f563d8a92b9cf4d4e280e6cf75ec995
Sumanth1997 <sumanthmylar@gmail.com>
2017-08-05 01:06:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,8 @@ console.log('Loaded!');
 
 
 //chane the text
-var element=document.getElementbyid('main-text');
+var element=document.getElementById('main-text'
+);
 
 
 element.innerHTML = 'new value';
