7a73552e2cd3bfb2015e84c362f23f2c616af2d4
Sumanth1997 <sumanthmylar@gmail.com>
2017-08-05 01:19:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,6 +10,10 @@ element.innerHTML = 'new value';
 
 //move the element
 var img = document.getElementById('madi');
+function moveRight () {
+ marginLeft = marginLeft+ 10;
+ img.style.marginLeft=marginLeft+ 'px';
+}
    img.onclick = function () {
-       img.style.marginLeft = '100px';
+       var interval = setInterval(marginRight, 100);
    };
