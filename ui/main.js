be35a77df190710e30a106f4d309f52f244607a9
Sumanth1997 <sumanthmylar@gmail.com>
2017-08-09 22:50:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,7 +17,6 @@ button.onclick = function () {
     //not done yet
   };
   //make a request
-  request.open('GET','http://sumanthmylar.imad.hasura-app.io/',true);
+  request.open('GET','http://sumanthmylar.imad.hasura-app.io/counter',true);
   request.send('null');
- 
 };
