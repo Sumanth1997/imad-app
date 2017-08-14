baa627b05debb9a601ae89044535eb9ab3c8575f
Sumanth1997 <sumanthmylar@gmail.com>
2017-08-14 21:51:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,14 +27,30 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function() {
-    //make a request to the server and send the name
-    
-    //capture a list of name and render it as a list
-    var names=['name1','name2','name3','name4'];
+      //create a request to a counter endpoint
+  var request = new XMLHttpRequest();
+  
+  //capture a response and share it in a variable
+  request.onreadystatechange= function() {
+    if(request.readyState === XMLHttpRequest.DONE) {
+        //take action
+        if(request.status === 200) {
+                //capture a list of name and render it as a list
+    var names=request.responseText;
+    names=JSON.parse(names);
     var list='';
     for(var i=0;i<names.length;i++) {
         list +='<li>'+names[i]+'</li>';
     }
     var ul=document.getElementById('namelist');
     ul.innerHTML = list;
+            
+        }
+    }
+    //not done yet
+  };
+  //make a request
+  request.open('GET','http://sumanthmylar.imad.hasura-app.io/counter'+name,true);
+  request.send(null);
 };
+    //make a request to the server and send the name
